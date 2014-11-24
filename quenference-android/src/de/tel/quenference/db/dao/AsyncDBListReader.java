fe769e16fd6f68e4afb91be0f04dc84ba5726925
zell.christopher <zelldon91@gmail.com>
2014-11-24 08:58:58 +0100

    added documentation to AsyncDBListReader

--- quenference-android/src/de/tel/quenference/db/dao/AsyncDBListReader.java
+++ quenference-android/src/de/tel/quenference/db/dao/AsyncDBListReader.java
@@ -23,87 +23,178 @@ import de.tel.quenference.db.ConferenceDBHelper;
 import java.util.ArrayList;
 import java.util.List;
 
+//TODO replace fields with SQLQuery class
+
 /**
+ * Represents the AsyncDBListReader which reads asynchronous from the database a
+ * list as result of a SQL Query.
+ *
  * @author Christopher Zell <zelldon91@googlemail.com>
  */
 public class AsyncDBListReader extends AsyncTask<Context, Void, List> {
 
-    private List values;
-    private String[] columns;
-    private String tableName;
-    private CursorExtractor extract;
-    private PostExecuteJob postJob;
-    //OPTIONAL
-    private String selection;
-    private String[] selectionArgs;
-    private String groupBy;
-    private String having;
-    private String orderBy;
+  /**
+   * The list which contains the elements after the SQL query was executed.
+   */
+  private List values;
+  
+  /**
+   * The columns which are requested in the SQL query.
+   */
+  private String[] columns;
+  
+  /**
+   * The table name on which the query will be executed.
+   */
+  private String tableName;
+  
+  /**
+   * The extractor which will be used to extract the values from the result set.
+   */
+  private CursorExtractor extract;
+  
+  /**
+   * The job which will be executed after the SQL query was executed.
+   */
+  private PostExecuteJob postJob;
+  
+  //================================================================
+  //============================OPTIONAL============================
+  //================================================================
+  
+  /**
+   * The selection or also named where clause.
+   */
+  private String selection;
+  
+  /**
+   * The arguments for the where clause.
+   */
+  private String[] selectionArgs;
+  
+  /**
+   * The group by clause for the SQL Statement.
+   */
+  private String groupBy;
+  
+  /**
+   * The having clause for the SQL Statement.
+   */
+  private String having;
+  
+  /**
+   * The order by clause for the SQL Statement.
+   */
+  private String orderBy;
 
-    public AsyncDBListReader(List values, String[] columns, String tableName,
-                             CursorExtractor extract, PostExecuteJob postJob) {
-        this.values = values;
-        this.columns = columns;
-        this.tableName = tableName;
-        this.extract = extract;
-        this.postJob = postJob;
+  /** 
+   * The ctor to create the AsyncDBListReader object to read
+   * asynchronous from the database a list as result of the given SQL Query. 
+   * 
+   * @param values              the list which will be used to save the result
+   * @param columns             the columns which are requested in the SQL query
+   * @param tableName           the table name on which the query will be executed.
+   * @param extract             the extractor which will be used to extract the values from the result set
+   * @param postJob             the job which will be executed after the SQL query was executed
+   */
+  public AsyncDBListReader(List values, String[] columns, String tableName,
+          CursorExtractor extract, PostExecuteJob postJob) {
+    this.values = values;
+    this.columns = columns;
+    this.tableName = tableName;
+    this.extract = extract;
+    this.postJob = postJob;
+  }
+
+  /**   
+   * The ctor to create the AsyncDBListReader object to read
+   * asynchronous from the database a list as result of the given SQL Query. 
+   * 
+   * @param values              the list which will be used to save the result
+   * @param columns             the columns which are requested in the SQL query
+   * @param tableName           the table name on which the query will be executed.
+   * @param extract             the extractor which will be used to extract the values from the result set
+   * @param postJob             the job which will be executed after the SQL query was executed
+   * @param selection           the selection or also named where clause
+   * @param selectionArgs       the arguments for the where clause
+   * @param groupBy             the group by clause for the SQL Statement
+   * @param having              the having clause for the SQL Statement
+   * @param orderBy             the order by clause for the SQL Statement
+   */
+  public AsyncDBListReader(List values, String[] columns, String tableName,
+          CursorExtractor extract, PostExecuteJob postJob, String selection,
+          String[] selectionArgs, String groupBy, String having, String orderBy) {
+    this.values = values;
+    this.columns = columns;
+    this.tableName = tableName;
+    this.extract = extract;
+    this.selection = selection;
+    this.selectionArgs = selectionArgs;
+    this.groupBy = groupBy;
+    this.having = having;
+    this.orderBy = orderBy;
+    this.postJob = postJob;
+  }
+
+  @Override
+  protected List doInBackground(Context... context) {
+    if (columns == null || extract == null || tableName == null
+            || context == null || context.length == 0) {
+      throw new IllegalStateException();
     }
 
-    public AsyncDBListReader(List values, String[] columns, String tableName,
-                             CursorExtractor extract, PostExecuteJob postJob, String selection,
-                             String[] selectionArgs, String groupBy, String having, String orderBy) {
-        this.values = values;
-        this.columns = columns;
-        this.tableName = tableName;
-        this.extract = extract;
-        this.selection = selection;
-        this.selectionArgs = selectionArgs;
-        this.groupBy = groupBy;
-        this.having = having;
-        this.orderBy = orderBy;
-        this.postJob = postJob;
+    if (values == null) {
+      values = new ArrayList();
     }
 
-
-    @Override
-    protected List doInBackground(Context... context) {
-        if (columns == null || extract == null || tableName == null
-                || context == null || context.length == 0) {
-            throw new IllegalStateException();
-        }
-
-        if (values == null) {
-            values = new ArrayList();
-        }
-
-        final ConferenceDBHelper DB_HELPER = new ConferenceDBHelper(context[0]);
-        SQLiteDatabase db = DB_HELPER.getReadableDatabase();
-        Cursor c = db.query(tableName, columns, selection, selectionArgs, groupBy, having, orderBy);
-        c.moveToFirst();
-        while (!c.isAfterLast()) {
-            Object o = extract.extract(c);
-            values.add(o);
-            c.moveToNext();
-        }
-        c.close();
-        DB_HELPER.close();
-
-        return values;
+    final ConferenceDBHelper DB_HELPER = new ConferenceDBHelper(context[0]);
+    SQLiteDatabase db = DB_HELPER.getReadableDatabase();
+    Cursor c = db.query(tableName, columns, selection, selectionArgs, groupBy, having, orderBy);
+    c.moveToFirst();
+    while (!c.isAfterLast()) {
+      Object o = extract.extract(c);
+      values.add(o);
+      c.moveToNext();
     }
+    c.close();
+    DB_HELPER.close();
 
-    @Override
-    protected void onPostExecute(List result) {
-        super.onPostExecute(result);
-        postJob.doJob(result);
-    }
+    return values;
+  }
 
-    public interface CursorExtractor {
+  @Override
+  protected void onPostExecute(List result) {
+    super.onPostExecute(result);
+    postJob.doJob(result);
+  }
 
-        public Object extract(Cursor c);
-    }
+  /**
+   * Represents an CursorExtractor which will be used
+   * for extracting a value or a class with his fields from a 
+   * database cursor.
+   */
+  public interface CursorExtractor {
 
-    public interface PostExecuteJob {
+    /**
+     * The extract method which will be used to extract the values.
+     * 
+     * @param c           the cursor which contains the values
+     * @return            the extracted object
+     */
+    public Object extract(Cursor c);
+  }
 
-        public void doJob(List result);
-    }
+  /**
+   * Represents the PostExecuteJob which will be used to execute a job/task after
+   * the SQL query was executed.
+   */
+  public interface PostExecuteJob {
+
+    /**
+     * The job which will be executed.
+     * 
+     * @param result      the result from the executed SQL query
+     */
+    public void doJob(List result);
+  }
 }
