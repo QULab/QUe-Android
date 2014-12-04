f27237d1033ad82ef45d79f8cb390f97a0d989da
zell.christopher <zelldon91@gmail.com>
2014-12-04 13:27:47 +0100

    #4 added null checks and also refactored the AsyncGetRequester

--- quenference-android/src/de/tel/quenference/async/AsyncGETRequester.java
+++ quenference-android/src/de/tel/quenference/async/AsyncGETRequester.java
@@ -35,15 +35,14 @@ import org.json.JSONException;
 import org.json.JSONObject;
 
 /**
- * Represents a async GET requester. 
- * For given URL's the AsyncTask will execute GET requests and 
- * returns the result of each request as JSONObject in given PostExecuteJob
- * doJob method call. Means to executed the AsyncTask, the caller needs to
- * pass into the task the URL's to request and a PostExecuteJob Object which will
- * be used to return the values or used if an exception appears.
- * If the Task was successful the doJob Method will be called of the 
+ * Represents a async GET requester. For given URL's the AsyncTask will execute
+ * GET requests and returns the result of each request as JSONObject in given
+ * PostExecuteJob doJob method call. Means to executed the AsyncTask, the caller
+ * needs to pass into the task the URL's to request and a PostExecuteJob Object
+ * which will be used to return the values or used if an exception appears. If
+ * the Task was successful the doJob Method will be called of the
  * PostExecutedJob, if not the doException will be called.
- * 
+ *
  * @author Christopher Zell <zelldon91@googlemail.com>
  */
 public class AsyncGETRequester extends AsyncTask<GetRequestInfo, Void, List<JSONObject>> {
@@ -56,7 +55,6 @@ public class AsyncGETRequester extends AsyncTask<GetRequestInfo, Void, List<JSON
    * The error log message.
    */
   private static final String ERROR_LOG_MSG = "Oops, the HTTP-GET request failed! Statuscode: %d";
-  
   /**
    * The exception log message.
    */
@@ -69,12 +67,10 @@ public class AsyncGETRequester extends AsyncTask<GetRequestInfo, Void, List<JSON
    * The credentials for basic authentication.
    */
   private UsernamePasswordCredentials credentials;
-  
   /**
    * The string for the If-None-Match Http-header.
    */
   private static final String HEADER_IF_NONE_MATCH = "If-None-Match";
-  
   /**
    * The string for the Etag Http-header.
    */
@@ -93,7 +89,7 @@ public class AsyncGETRequester extends AsyncTask<GetRequestInfo, Void, List<JSON
    * The ctor of the AsyncJSONSender
    *
    * @param job the job which will be executed after sending the objects
-   * @param credentials   the user credentials to make the get request
+   * @param credentials the user credentials to make the get request
    */
   public AsyncGETRequester(PostExecuteJob job, UsernamePasswordCredentials credentials) {
     this.job = job;
@@ -102,56 +98,60 @@ public class AsyncGETRequester extends AsyncTask<GetRequestInfo, Void, List<JSON
 
   @Override
   protected List<JSONObject> doInBackground(GetRequestInfo... urls) {
-    HttpClient client = new DefaultHttpClient();
     List<JSONObject> result = new ArrayList<JSONObject>();
     if (urls != null) {
       for (int i = 0; i < urls.length; i++) {
         String url = urls[i].getUrl();
-        HttpGet get = new HttpGet(url);
-        String etag = urls[i].getEtag();
-        if (etag != null && !etag.isEmpty()) {
-          get.setHeader(HEADER_IF_NONE_MATCH, etag);
-        }
-
-        if (credentials != null) {
-          get.addHeader(BasicScheme.authenticate(credentials, HTTP.UTF_8, false));
-        }
-
-        try {
-          HttpResponse response = client.execute(get);
-          int statusCode = response.getStatusLine().getStatusCode();
-          if (response == null || statusCode >= 400) {
-            Log.e(AsyncGETRequester.class.getName(), String.format(ERROR_LOG_MSG, response.getStatusLine().getStatusCode()));
-            job.doExeptionHandling(null);
-          } else {
-            if (statusCode != 304) {
-              HttpEntity entity = response.getEntity();
-              if (entity != null && entity.getContentType().getValue().contains(CONTENT_TYPE)) {
-                try {
-                  JSONObject object = new JSONObject(EntityUtils.toString(entity));
-                  result.add(object);
-                } catch (JSONException ex) {
-                  Log.e(AsyncGETRequester.class.getName(), EXECEPTION_LOG_MSG, ex);
-                }
-              }
-            }
-            Header responseEtag = response.getFirstHeader(HEADER_ETAG);
-            if (responseEtag != null) {
-              String newEtag = responseEtag.getValue();
-              if (newEtag != null && !newEtag.equalsIgnoreCase(etag))
-                job.handleNewEtag(url, newEtag);
-            }
+        if (url != null) {
+          HttpGet get = new HttpGet(url);
+          String etag = urls[i].getEtag();
+          if (etag != null && !etag.isEmpty()) {
+            get.setHeader(HEADER_IF_NONE_MATCH, etag);
           }
-        } catch (ClientProtocolException ex) {
-          Log.e(AsyncGETRequester.class.getName(), EXECEPTION_LOG_MSG, ex);
-          job.doExeptionHandling(ex);
-        } catch (IOException ex) {
-          Log.e(AsyncGETRequester.class.getName(), EXECEPTION_LOG_MSG, ex);
-          job.doExeptionHandling(ex);
+
+          if (credentials != null) {
+            get.addHeader(BasicScheme.authenticate(credentials, HTTP.UTF_8, false));
+          }
+          executeGetRequest(get, result);
         }
       }
     }
     return result;
+  }
+
+  private void executeGetRequest(HttpGet get, List<JSONObject> result) {
+    HttpClient client = new DefaultHttpClient();
+    try {
+      HttpResponse response = client.execute(get);
+      int statusCode = response.getStatusLine().getStatusCode();
+      if (response == null || statusCode >= 400) {
+        Log.e(AsyncGETRequester.class.getName(), String.format(ERROR_LOG_MSG, response.getStatusLine().getStatusCode()));
+        job.doExeptionHandling(null);
+      } else {
+        if (statusCode != 304) {
+          HttpEntity entity = response.getEntity();
+          if (entity != null && entity.getContentType().getValue().contains(CONTENT_TYPE)) {
+            try {
+              JSONObject object = new JSONObject(EntityUtils.toString(entity));
+              result.add(object);
+            } catch (JSONException ex) {
+              Log.e(AsyncGETRequester.class.getName(), EXECEPTION_LOG_MSG, ex);
+            }
+          }
+        }
+        Header responseEtag = response.getFirstHeader(HEADER_ETAG);
+        if (responseEtag != null) {
+          String newEtag = responseEtag.getValue();
+          if (newEtag != null &&
+              !newEtag.equalsIgnoreCase(get.getFirstHeader(HEADER_IF_NONE_MATCH).getValue())) {
+            job.handleNewEtag(get.getURI().toString(), newEtag);
+          }
+        }
+      }
+    } catch (IOException ex) {
+      Log.e(AsyncGETRequester.class.getName(), EXECEPTION_LOG_MSG, ex);
+      job.doExeptionHandling(ex);
+    }
   }
 
   @Override
@@ -183,12 +183,12 @@ public class AsyncGETRequester extends AsyncTask<GetRequestInfo, Void, List<JSON
      * @param t the throwable which cause the error
      */
     public void doExeptionHandling(Throwable t);
-    
+
     /**
      * Method should handle the new etag.
-     * 
-     * @param oldEtag       the corresponding url
-     * @param newEtag       the new etag
+     *
+     * @param oldEtag the corresponding url
+     * @param newEtag the new etag
      */
     public void handleNewEtag(String url, String newEtag);
   }
