6986224e17ec22df91b88f9daf4873c56b619a9f
zell.christopher <zelldon91@gmail.com>
2015-01-05 12:27:11 +0100

    added comments to the SessionViewFragment

--- quenference-android/src/org/que/activities/fragments/SessionViewFragment.java
+++ quenference-android/src/org/que/activities/fragments/SessionViewFragment.java
@@ -32,104 +32,121 @@ import org.que.db.dao.SQLQuery;
 import org.que.db.entities.SessionEntity;
 
 /**
+ * Represents the Fragment for the session view.
+ *
  * @author Christopher Zell <zelldon91@googlemail.com>
  */
 public class SessionViewFragment extends FavoriteMenuFragment {
 
-    public static final String ARG_SESSIONVIEW_FRAGMENT = "sessionview_arg";
-    private static final String TAG_SESSIONVIEW = "sessionview_tag";
-    private SessionEntity session;
-    private TextView title, chairs;
+  /**
+   * The argument key for the fragment.
+   */
+  public static final String ARG_SESSIONVIEW_FRAGMENT = "sessionview_arg";
+  
+  /**
+   * The tag for the session view.
+   */
+  private static final String TAG_SESSIONVIEW = "sessionview_tag";
+  
+  /**
+   * The session entity which contains the session informations.
+   */
+  private SessionEntity session;
+  
+  /**
+   * The Textviews which shows the session informations.
+   */
+  private TextView title, chairs;
 
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
+  @Override
+  public void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
 
-        if (null == savedInstanceState) {
-            savedInstanceState = getArguments();
-        }
-
-        if (null != savedInstanceState) {
-            session = (SessionEntity) savedInstanceState.getSerializable(TAG_SESSIONVIEW);
-        }
-
-        if (null == session) {
-            session = (SessionEntity) savedInstanceState.getSerializable(ARG_SESSIONVIEW_FRAGMENT);
-        }
-
+    if (null == savedInstanceState) {
+      savedInstanceState = getArguments();
     }
 
-    @Override
-    public void onResume() {
-        super.onResume();
-        setFavIcon(favorited);
+    if (null != savedInstanceState) {
+      session = (SessionEntity) savedInstanceState.getSerializable(TAG_SESSIONVIEW);
     }
 
-    @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        View rootView = inflater.inflate(R.layout.fragment_sessionview, container, false);
-        title = ((TextView) rootView.findViewById(R.id.session_title));
-        title.setText(session.getTitle());
-        chairs = ((TextView) rootView.findViewById(R.id.session_chairs));
-        String chair = session.getChair();
-        String coChair = session.getCoChair();
-        if (chair.equals("null")) {
-            chair = "N.A.";
-        }
-        if (coChair.equals("null")) {
-            coChair = "N.A.";
-        }
-        chairs.setText(String.format(getString(R.string.session_chairs),
-                chair,
-                coChair));
-
-        Fragment frg = new SessionSpeechesList();
-        Bundle args = new Bundle();
-        String select = ConferenceDBContract.ConferencePaper.COLUMN_NAME_SESSION + SQLQuery.SQL_SEARCH_EQUAL;
-        SQLQuery query = new SQLQuery(select,
-                                      Entity.PAPER,
-                                      ConferenceDAO.PAPER_COLUMNS);
-
-        query.setSelectionArgs(session.getId().toString());
-        args.putSerializable(SearchTabFragmentViewPager.ARG_SEARCH_QUERY, query);
-        args.putSerializable(SearchTabFragmentViewPager.ARG_SEARCH_FRAGMENT, SearchTabFragmentViewPager.TabSearch.PAPER);
-        frg.setArguments(args);
-        FragmentManager mgr = ((FragmentActivity) getActivity()).getSupportFragmentManager();
-        mgr.beginTransaction().add(R.id.session_speeches, frg).commit();
-        //Toast.makeText(getActivity(), "create "+session.getFavorite().toString(), Toast.LENGTH_SHORT).show();
-        getActivity().setTitle(session.getTitle());
-        setFavIcon(session.getFavorite());
-
-        return rootView;
+    if (null == session) {
+      session = (SessionEntity) savedInstanceState.getSerializable(ARG_SESSIONVIEW_FRAGMENT);
     }
 
-    @Override
-    public void onSaveInstanceState(Bundle outState) {
-        super.onSaveInstanceState(outState);
-        outState.putSerializable(TAG_SESSIONVIEW, session);
-    }
+  }
 
-    @Override
-    public void onDestroy() {
-        super.onDestroy();
-    }
+  @Override
+  public void onResume() {
+    super.onResume();
+    setFavIcon(favorited);
+  }
 
-    @Override
-    public SQLQuery getFavoriteUpdateSQLQuery() {
-        String select = ConferenceDBContract.ConferenceSession.COLUMN_NAME_ID + SQLQuery.SQL_SEARCH_EQUAL;
-        SQLQuery query = new SQLQuery(select, Entity.SESSION, ConferenceDAO.PAPER_COLUMNS);
-        query.setSelectionArgs(session.getId().toString());
-        return query;
+  @Override
+  public View onCreateView(LayoutInflater inflater, ViewGroup container,
+          Bundle savedInstanceState) {
+    View rootView = inflater.inflate(R.layout.fragment_sessionview, container, false);
+    title = ((TextView) rootView.findViewById(R.id.session_title));
+    title.setText(session.getTitle());
+    chairs = ((TextView) rootView.findViewById(R.id.session_chairs));
+    String chair = session.getChair();
+    String coChair = session.getCoChair();
+    if (chair.equals("null")) {
+      chair = "N.A.";
     }
+    if (coChair.equals("null")) {
+      coChair = "N.A.";
+    }
+    chairs.setText(String.format(getString(R.string.session_chairs),
+            chair,
+            coChair));
 
-    @Override
-    protected String getFavoriteColumnName() {
-        return ConferenceDBContract.ConferenceSession.COLUMN_NAME_FAVORITE;
-    }
+    Fragment frg = new SessionSpeechesList();
+    Bundle args = new Bundle();
+    String select = ConferenceDBContract.ConferencePaper.COLUMN_NAME_SESSION + SQLQuery.SQL_SEARCH_EQUAL;
+    SQLQuery query = new SQLQuery(select,
+            Entity.PAPER,
+            ConferenceDAO.PAPER_COLUMNS);
 
-    @Override
-    protected Serializable getEntity() {
-        return session;
-    }
+    query.setSelectionArgs(session.getId().toString());
+    args.putSerializable(SearchTabFragmentViewPager.ARG_SEARCH_QUERY, query);
+    args.putSerializable(SearchTabFragmentViewPager.ARG_SEARCH_FRAGMENT, SearchTabFragmentViewPager.TabSearch.PAPER);
+    frg.setArguments(args);
+    FragmentManager mgr = ((FragmentActivity) getActivity()).getSupportFragmentManager();
+    mgr.beginTransaction().add(R.id.session_speeches, frg).commit();
+    //Toast.makeText(getActivity(), "create "+session.getFavorite().toString(), Toast.LENGTH_SHORT).show();
+    getActivity().setTitle(session.getTitle());
+    setFavIcon(session.getFavorite());
+
+    return rootView;
+  }
+
+  @Override
+  public void onSaveInstanceState(Bundle outState) {
+    super.onSaveInstanceState(outState);
+    outState.putSerializable(TAG_SESSIONVIEW, session);
+  }
+
+  @Override
+  public void onDestroy() {
+    super.onDestroy();
+  }
+
+  @Override
+  public SQLQuery getFavoriteUpdateSQLQuery() {
+    String select = ConferenceDBContract.ConferenceSession.COLUMN_NAME_ID + SQLQuery.SQL_SEARCH_EQUAL;
+    SQLQuery query = new SQLQuery(select, Entity.SESSION, ConferenceDAO.PAPER_COLUMNS);
+    query.setSelectionArgs(session.getId().toString());
+    return query;
+  }
+
+  @Override
+  protected String getFavoriteColumnName() {
+    return ConferenceDBContract.ConferenceSession.COLUMN_NAME_FAVORITE;
+  }
+
+  @Override
+  protected Serializable getEntity() {
+    return session;
+  }
 }
